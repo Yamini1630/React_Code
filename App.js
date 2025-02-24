dbb635173e7e937d9eaf0637931c83806499f629
Yamini <yaminiramalingam30@gmail.com>
2025-02-24 21:40:40 +0530

    commit final

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-//<div>
-   // <div id="child">
-     //   <h1></h1>
-   // </div>
-//</div>
+import React from "react";
+import ReactDOM from "react-dom/client";
+const Header=()=>{
+    return(
+        <div className="header">
+    <div className="logo">
+        <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"></img>
+    </div>
+    <div className="nav-items">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+        </ul>
+    </div>
+    </div>
+)}
+
+const AppLayout=()=>{
+    return <div className="app">
+        <Header />
+    </div>
+}
 
 
 
-const heading=React.createElement("h1",{},React.createElement("div",{id:"parent"},[React.createElement("h1",{},"H1 tag"),
-    React.createElement("h2",{},"H2 tag")]));
 
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout />);
 
-
-    const root=ReactDOM.createRoot(document.getElementById("root"))
-    root.render(heading);
